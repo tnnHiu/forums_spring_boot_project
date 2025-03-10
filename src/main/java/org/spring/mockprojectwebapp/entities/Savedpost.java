bc405768ddf642503b60b98e6e2c46fd5a3adf1a
nguyetque32 <ttnque.20it8@vku.udn.vn>
2025-03-10 09:14:20 +0700

    update styles.css

---
+++ src/main/java/org/spring/mockprojectwebapp/entities/Savedpost.java
@@ -0,0 +1,36 @@
+package org.spring.mockprojectwebapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "savedposts")
+public class Savedpost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "saved_post_id")
+    private int savedPostId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
