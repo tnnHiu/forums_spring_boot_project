3b0acbc4b6fe2403ce79f1e69c99d83e01edba5c
Trinh Nguyen Nhat Hieu <trinhnguyennhathieu@gmail.com>
2025-03-14 10:52:28 +0700

    fix code

--- src/main/java/org/spring/mockprojectwebapp/services/UserService.java
+++ src/main/java/org/spring/mockprojectwebapp/services/UserService.java
@@ -3,5 +3,4 @@ package org.spring.mockprojectwebapp.services;
 import org.spring.mockprojectwebapp.entities.User;
 
 public interface UserService {
-    User login(String email, String password);
 }
--- src/main/java/org/spring/mockprojectwebapp/services/implement/UserServiceImpl.java
+++ src/main/java/org/spring/mockprojectwebapp/services/implement/UserServiceImpl.java
@@ -1,30 +1,14 @@
 package org.spring.mockprojectwebapp.services.implement;
 
-import org.spring.mockprojectwebapp.dtos.LoginDTO;
-import org.spring.mockprojectwebapp.entities.User;
-import org.spring.mockprojectwebapp.repositories.UserRepository;
+
 import org.spring.mockprojectwebapp.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public User login(String email, String password) {
-        return userRepository.findByEmail(email);
-    }
-//
-//    @Override
-//    public User login() {
-//        return userRepository.findByEmail(loginDto.getEmail()).filter(user -> user
-//                .getPassword().equals(loginDto.getPassword()));
-//    }'
 
 }
 
