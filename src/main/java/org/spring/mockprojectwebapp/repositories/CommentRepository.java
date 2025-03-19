02f950475bc66fd1c3b77a9afa4fd6aba607a62e
Trinh Nguyen Nhat Hieu <trinhnguyennhathieu@gmail.com>
2025-03-19 09:20:20 +0700

    fix comment and load more comment

--- src/main/java/org/spring/mockprojectwebapp/repositories/CommentRepository.java
+++ src/main/java/org/spring/mockprojectwebapp/repositories/CommentRepository.java
@@ -15,4 +15,5 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     @Query("SELECT c FROM Comment c " + "JOIN FETCH c.user u " + "WHERE c.post.id = :postId " + "ORDER BY c.createdAt DESC")
     List<Comment> findCommentsByPostIdWithUser(@Param("postId") int postId);
 
+    int countByPostId(int postId);
 }
